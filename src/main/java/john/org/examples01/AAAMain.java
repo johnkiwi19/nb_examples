e567f5f2fe50fa91bb97b3ac3f94180b6159696b
john <John_Mallett@john.com>
2021-12-28 10:23:34 +1300

    Added a main Classo

---
+++ src/main/java/john/org/examples01/AAAMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package john.org.examples01;
+
+/**
+ *
+ * @author john
+ */
+public class AAAMain {
+
+    public AAAMain() {
+        System.out.println("Hi From Examples ");
+    }
+    
+    public static void main(String[] args) {
+        new AAAMain();
+    }
+}
