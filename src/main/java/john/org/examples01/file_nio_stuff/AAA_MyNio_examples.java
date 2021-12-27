098a50041a86cdd7b7ad5820486042cf4988121d
john <John_Mallett@john.com>
2021-12-28 10:58:06 +1300

    Renaimed the main class to AAA as this will place the class at The top of the list of Classes

---
+++ src/main/java/john/org/examples01/file_nio_stuff/AAA_MyNio_examples.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package john.org.examples01.file_nio_stuff;
+
+/**
+ *
+ * @author john
+ */
+public class AAA_MyNio_examples {
+
+    public AAA_MyNio_examples() {
+        System.out.print("Class ::: "+this.getClass().getName()+ " ");
+        System.out.println("This is a class for nio exapmles ");
+        new C01PathEaxmple();
+    }
+    
+}
