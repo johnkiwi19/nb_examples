9deb18abb7da2b0f79d102dd8384ddbf12e4a3a8
john <John_Mallett@john.com>
2021-12-28 10:39:44 +1300

    Added a class for doing NIO examples

---
+++ src/main/java/john/org/examples01/file_nio_stuff/MyNio_examples.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package john.org.examples01.file_nio_stuff;
+
+/**
+ *
+ * @author john
+ */
+public class MyNio_examples {
+
+    public MyNio_examples() {
+        
+    }
+    
+}
