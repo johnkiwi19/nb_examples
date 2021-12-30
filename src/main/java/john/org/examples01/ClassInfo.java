7ef6855a5fb0f5d651375d008b64ea4dad1da143
john <John_Mallett@john.com>
2021-12-31 02:56:22 +1300

    Having a break from this for a while

--- src/main/java/john/org/examples01/ClassInfo.java
+++ src/main/java/john/org/examples01/ClassInfo.java
@@ -13,23 +13,36 @@ public class ClassInfo {
     String s;
     Object t;
     boolean show;
-    
+
     public ClassInfo() {
 
     }
 
     public ClassInfo(Object t) {
         this.t = t;
-        showClass(t);
         this.show = true;
-        
+        showClass(t);
+
     }
 
     private void showClass(Object t1) {
-        if (show){
-        System.out.println("Class    :: " + t1.getClass().getCanonicalName());
+        if (show) {
+            System.out.println("Class    :: " + t1.getClass().getCanonicalName());
         }
     }
-    
 
+    public void on() {
+        show = true;
+    }
+
+    public void off() {
+        show = false;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassInfo{" + "s=" + s + ", t=" + t + ", show=" + show + '}';
+    }
+    
+    
 }
