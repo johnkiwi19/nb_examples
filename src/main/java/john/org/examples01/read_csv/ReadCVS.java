2de962dace6bac271b5ca63dcdd34372dfd23888
john <John_Mallett@john.com>
2021-12-31 02:59:16 +1300

    Add a string for the filename

---
+++ src/main/java/john/org/examples01/read_csv/ReadCVS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package john.org.examples01.read_csv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ *
+ * @author john
+ */
+public class ReadCVS {
+
+    public ReadCVS() {
+        System.out.println("This class will reatd a cvs file");
+        
+        String name = "student.csv";
+        
+    }
+    
+    
+    
+}
