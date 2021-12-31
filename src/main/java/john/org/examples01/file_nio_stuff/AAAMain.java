7cfff1daddaecfdb9eb099ce089467620cd79c69
john <John_Mallett@john.com>
2021-12-31 21:46:25 +1300

    Added a main class for Path

---
+++ src/main/java/john/org/examples01/file_nio_stuff/AAAMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package john.org.examples01.file_nio_stuff;
+
+/**
+ *
+ * @author john
+ */
+public class AAAMain {
+    public static void main(String[] args) {
+        System.out.println("The Path class");
+    }
+    
+}
